e9fc659181ff0f3bd9da1fa98734ef47e2347a6e
Rajkumar Chaudhari <chaudharir848@gmail.com>
2022-08-12 11:37:40 +0500

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const { src, watch, dest, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const purgecss = require("gulp-purgecss");
+
+function buildStyle() {
+  return src("ninja/**/*.scss")
+    .pipe(sass())
+    .pipe(purgecss({ content: ["*.html"] }))
+    .pipe(dest("css"));
+}
+
+function watchTask() {
+  watch(["ninja/**/*.scss","*.html"], buildStyle);
+}
+
+exports.default = series(buildStyle, watchTask);
